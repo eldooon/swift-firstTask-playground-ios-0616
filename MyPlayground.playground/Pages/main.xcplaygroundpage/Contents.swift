b182aff5f623b83fde50d3b5c1eba2aeeef657da
eldooon <eldonchan91@gmail.com>
2016-07-05 14:24:07 -0400

    done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,8 @@
 /*: question1
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
-// write your code here
+//Should be a var because the balance may change over time, and int because we can do math operation with it
+var currentBalance = 1738.00
 
 
 
@@ -22,48 +23,48 @@
 /*: question2
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
-// write your code here
-
+// using let because we won't change the puppy's name
+let puppyName = "Bella"
 
 
 
 /*: question3
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
-// write your code here
-
+// printing puppy name
+print (puppyName)
 
 
 
 /*: question4
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
-// write your code here
-
+// printing puppy name with string
+print ("I just got a new puppy named \(puppyName) and she is awesome!")
 
 
 
 /*: question5
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
-// write your code here
-
+// printing string of bank account
+print ("I have $\(currentBalance) in my bank account.")
 
 
 
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
-// write your code here
-
-
+// adding birthday money and update balance
+currentBalance += 100
+print ("I have $\(currentBalance) in my bank account.")
 
 
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
-// write your code here
-
+// we get an error because Bella's name is a constant
+puppyName = "Not Bella"
 
 
 
